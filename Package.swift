02f2a354b8c407ede529bdf4cb6950251d986ae3
Marcus Painter <marcus@marcuspainter.com>
2024-03-10 04:43:09 +0000

    Privacy Policy.

--- Package.swift
+++ Package.swift
@@ -24,11 +24,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "VecLab",
-            dependencies:
-                [
-                ]),
+            dependencies: [],
+            resources: [.copy("PrivacyInfo.xcprivacy")]
+        ),
         .testTarget(
             name: "VecLabTests",
-            dependencies: ["VecLab"]),
+            dependencies: ["VecLab"]
+        )
     ]
 )
