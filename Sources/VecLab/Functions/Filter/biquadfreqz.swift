86817448ff2311e0038ecfb78b94cee3bf78998e
Marcus Painter <marcus@marcuspainter.com>
2024-08-28 00:28:18 +0100

    Create biquadfreqz.swift

---
+++ Sources/VecLab/Functions/Filter/biquadfreqz.swift
@@ -0,0 +1,54 @@
+//
+//  File.swift
+//
+//
+//  Created by Marcus Painter on 27/08/2024.
+//
+
+import Foundation
+
+/// Transfer function for biquad filter coefficients.
+/// - Parameters:
+///   - b: b coefficients.
+///   - a: a coefficients.
+/// - Returns: Complex transfer function.
+public func biquadfreqz(_ b: RealArray, _ a: RealArray) -> ComplexArray {
+    let b0 = b[0]
+    let b1 = b[1]
+    let b2 = b[2]
+    let a0 = a[0]
+    let a1 = a[1]
+    let a2 = a[2]
+    
+    let N = 512
+    let w = vector(0 ... N-1) * (.pi / Real(N))
+
+    // Compute trigonometric terms
+    let sin_half_w = sin(w / 2)
+    let sin_w = sin(w)
+    let sin_2w = sin(2 * w)
+
+    // Compute cosine terms using trigonometric identities
+    let cos_w =  1 - 2 * sin_half_w * sin_half_w
+    let cos_2w = 1 - 2 * sin_w * sin_w
+
+    // Real and imaginary parts of the numerator
+    let num_re = b0 + b1 * cos_w + b2 * cos_2w
+    let num_im =     -b1 * sin_w - b2 * sin_2w  // Corrected sign
+
+    // Real and imaginary parts of the denominator
+    let den_re = a0 + a1 * cos_w + a2 * cos_2w
+    let den_im =     -a1 * sin_w - a2 * sin_2w  // Corrected sign
+
+    // Compute the complex frequency response
+    let num = (num_re, num_im)
+    let den = (den_re, den_im)
+    let H = num / den
+    
+    return H
+}
+
+// https://dsp.stackexchange.com/questions/16885/how-do-i-manually-plot-the-frequency-response-of-a-bandpass-butterworth-filter-i/16911#16911
+// https://www.musicdsp.org/en/latest/Analysis/186-frequency-response-from-biquad-coefficients.html
+// from RBJ @ http://groups.google.com/group/comp.dsp/browse_frm/thread/8c0fa8d396aeb444/a1bc5b63ac56b686
+// https://dsp.stackexchange.com/questions/24222/iir-filter-calculating-the-phase-response
