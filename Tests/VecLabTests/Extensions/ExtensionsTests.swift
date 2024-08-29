95094b9ba6d75e258864b5d5205109fbc4fe8134
Marcus Painter <marcus@marcuspainter.com>
2024-08-29 17:36:41 +0100

    Add frac function

--- Tests/VecLabTests/Extensions/ExtensionsTests.swift
+++ Tests/VecLabTests/Extensions/ExtensionsTests.swift
@@ -136,6 +136,12 @@ class ExtensionsTests: XCTestCase {
         let expected: Complex =  (0.0, 4.0)
         XCTAssertEqual(result, expected, "Tuple Imaginary unit failed")
     }
+    
+    func testImaginaryTupleMultiply() throws {
+        let result = (1.0, 1.0) * (2.0, 2.0)
+        let expected = (0.0, 4.0)
+        XCTAssertEqual(result, expected, "Tuple multiplication failed")
+    }
 }
 
 
