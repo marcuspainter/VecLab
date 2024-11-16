16f6deb68f1aa014c6ffb87c377a474f7a363156
Marcus Painter <marcus@marcuspainter.com>
2024-11-16 04:39:29 +0000

    Rename exp2 to expeuler

--- Sources/VecLab/Functions/Trigonometry/expeuler.swift
+++ Sources/VecLab/Functions/Trigonometry/expeuler.swift
@@ -10,7 +10,7 @@ import Accelerate
 /// Returns a complex array from angles by Euler
 /// - Parameter x: Angles in radians
 /// - Returns: Complex array of (cos(x),  sin(x)) equivalent to exp(i \* x) by Euler's formula.
-public func exp2(x: RealArray) -> ComplexArray {
+public func expeuler(_ x: RealArray) -> ComplexArray {
     var angles = x
     // Copy initialization
     var cosines = angles
@@ -24,6 +24,6 @@ public func exp2(x: RealArray) -> ComplexArray {
 /// Returns a complex number from angle by Euler
 /// - Parameter x: Angle in radians
 /// - Returns: Complex number of (cos(x),  sin(x)) equivalent to exp(i \* x) by Euler's formula.
-public func exp2(x: Real) -> Complex {
+public func expeuler(_ x: Real) -> Complex {
     return (Darwin.cos(x), Darwin.sin(x))
 }
