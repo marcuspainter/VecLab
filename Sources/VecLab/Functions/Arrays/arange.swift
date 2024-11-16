ca4019b2b5324e3033a0d454a02d6fdba1927e16
Marcus Painter <marcus@marcuspainter.com>
2024-11-16 21:29:39 +0000

    Add Int to arange

--- Sources/VecLab/Functions/Arrays/arange.swift
+++ Sources/VecLab/Functions/Arrays/arange.swift
@@ -42,6 +42,31 @@ public func arange(_ start: Real, _ stop: Real, _ step: Real) -> RealArray {
     return result
 }
 
+/// Return evenly spaced values within a given interval.
+/// - Parameters:
+///   - start: Integer value.
+///   - stop: Integer value.
+///   - step: Integer value.
+/// - Returns: The array.
+public func arange(_ start: Int, _ stop: Int, _ step: Int = 1) -> [Int] {
+    guard step != 0 else {
+        fatalError("Step cannot be zero.")
+    }
+    
+    // Calculate the number of elements
+    let length = max(0, (stop - start + (step > 0 ? step - 1 : step + 1)) / step)
+    
+    var result = [Int](repeating: 0, count: length)
+    var current = start
+    
+    for i in 0..<length {
+        result[i] = current
+        current += step
+    }
+    
+    return result
+}
+
 // Case 1: Only 'stop' is provided, defaults are start = 0.0, step = 1.0
 // Case 2: 'start' and 'stop' are provided, default is step = 1.0
 // Case 3: 'start', 'stop', and 'step' are all provided
