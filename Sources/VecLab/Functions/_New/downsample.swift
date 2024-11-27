8b1e8a6c058ea57f2306f65bed0a4ddcd38d6f96
Marcus Painter <marcus@marcuspainter.com>
2024-11-27 16:05:55 +0000

    Add upsample, downsample

--- Sources/VecLab/Functions/_New/downsample.swift
+++ Sources/VecLab/Functions/_New/downsample.swift
@@ -5,23 +5,22 @@
 //  Created by Marcus Painter on 19/11/2024.
 //
 
-
 /// Decrease sample rate by integer factor.
 /// - Parameters:
 ///   - input: Real array.
 ///   - factor: Downsampling factor.
 ///   - phase: Starting offset. Default = 0.
 /// - Returns: Real array of the first (or phase) sample and then every nth samples.
-public func downsample(_ input: [Double], _ factor: Int, _ phase: Int = 0) -> [Double] {
-    guard factor > 0 else { return input }
+public func downsample(_ x: [Double], _ factor: Int, _ phase: Int = 0) -> [Double] {
+    guard factor > 0 else { return x }
     let adjustedPhase = phase % factor // Ensure phase is within 0..(factor-1)
-    let outputSize = (input.count - adjustedPhase + factor - 1) / factor // ceil((input.count - phase) / factor)
+    let outputSize = (x.count - adjustedPhase + factor - 1) / factor // ceil((input.count - phase) / factor)
     var result = [Double](repeating: 0.0, count: outputSize)
 
     var i = adjustedPhase
     var outputIndex = 0
-    while i < input.count {
-        result[outputIndex] = input[i]
+    while i < x.count {
+        result[outputIndex] = x[i]
         outputIndex += 1
         i += factor
     }
--- Sources/VecLab/Functions/_New/upsample.swift
+++ Sources/VecLab/Functions/_New/upsample.swift
@@ -11,18 +11,16 @@
 ///   - factor: Upsampling factor.
 ///   - phase: Starting offset. Default = 0.
 /// - Returns: Real array of n – 1 zeros between samples.
-public func upsample(_ input: [Double], _ factor: Int, _ phase: Int = 0) -> [Double] {
-    guard factor > 0 else { return input }
+public func upsample(_ x: RealArray, _ factor: Int, _ phase: Int = 0) -> [Double] {
+    guard factor > 0 else { return x }
     let adjustedPhase = phase % factor // Ensure phase is within 0..(factor-1)
-    let outputSize = input.count * factor
-    var result = [Double](repeating: 0.0, count: outputSize)
+    let outputSize = x.count * factor
+    var result = RealArray(repeating: 0.0, count: outputSize)
 
     var outputIndex = adjustedPhase
-    for i in 0 ..< input.count {
-        result[outputIndex] = input[i]
+    for i in 0 ..< x.count {
+        result[outputIndex] = x[i]
         outputIndex += factor
     }
     return result
 }
-
-
