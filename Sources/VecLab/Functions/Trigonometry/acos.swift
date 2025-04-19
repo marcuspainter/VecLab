89f3708aa42a5d4268d7aa9f77c2c3e00d093db7
Marcus Painter <marcus@marcuspainter.com>
2025-04-19 08:13:21 +0100

    Acos typo

--- Sources/VecLab/Functions/Trigonometry/acos.swift
+++ Sources/VecLab/Functions/Trigonometry/acos.swift
@@ -15,18 +15,19 @@ public func acos(_ x: RealArray) -> RealArray {
     return vForce.acos(x)
 }
 
-/// Inverse sine of complex number..
+/// Inverse cosine of complex number..
 /// - Parameter x: Sine of angle.
-/// - Returns: Inverse sin of x in the interval [0, π].
+/// - Returns: Inverse cosine of x in the interval [0, π].
 public func acos(_ x: Complex) -> Complex {
     // Matlab: -1i * log( z + 1i * sqrt(1 - z^2))
     return -Real.i * log(x + Real.i * sqrt(1.0 - x * x))
 }
 
 /// Inverse sine of complex array.
-/// - Parameter x: Sine of angles.
-/// - Returns: Inverse sin of x in the interval [0, π].
+/// - Parameter x: Cosine of angles.
+/// - Returns: Inverse cossin of x in the interval [0, π].
 public func acos(_ x: ComplexArray) -> ComplexArray {
     assertSameSize(x)
-    return -Real.i * log(x + Real.i * sqrt(1.0 - x * x))
+    let xx = x
+    return -Real.i * log(xx + Real.i * sqrt(1.0 - xx * xx))
 }
