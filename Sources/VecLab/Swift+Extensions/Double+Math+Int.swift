05c63b5dcab75455e2abb3673f48197573fd5da3
Marcus Painter <marcus@marcuspainter.com>
2025-04-19 06:07:48 +0100

    Create Double+Math+Int.swift

---
+++ Sources/VecLab/Swift+Extensions/Double+Math+Int.swift
@@ -0,0 +1,91 @@
+//
+//  Double+Math+Int.swift
+//  VecLab
+//
+//  Created by Marcus Painter on 19/04/2025.
+//
+/*
+extension Double {
+    
+    /// Integer addition..
+    /// - Parameters:
+    ///   - a: Real number.
+    ///   - b: Integer number.
+    /// - Returns: Raises `a` to the power of `b`.
+    @inlinable
+    public static func + (_ a: Double, _ b: Int) -> Double {
+        return a + Double(b)
+    }
+    
+    /// Integer addition..
+    /// - Parameters:
+    ///   - a: Real number.
+    ///   - b: Integer number.
+    /// - Returns: Raises `a` to the power of `b`.
+    @inlinable
+    public static func + (_ a: Int, _ b: Double) -> Double {
+        return Double(a) + b
+    }
+    
+    /// Integer subtraction..
+    /// - Parameters:
+    ///   - a: Real number.
+    ///   - b: Integer number.
+    /// - Returns: Raises `a` to the power of `b`.
+    @inlinable
+    public static func - (_ a: Double, _ b: Int) -> Double {
+        return a - Double(b)
+    }
+    
+    /// Integer subtraction..
+    /// - Parameters:
+    ///   - a: Integer number.
+    ///   - b: Real number.
+    /// - Returns: Raises `a` to the power of `b`.
+    @inlinable
+    public static func - (_ a: Int, _ b: Double) -> Double {
+        return Double(a) - b
+    }
+    
+    /// Integer multiplication..
+    /// - Parameters:
+    ///   - a: Real number.
+    ///   - b: Integer number.
+    /// - Returns: Raises `a` to the power of `b`.
+    @inlinable
+    public static func * (_ a: Double, _ b: Int) -> Double {
+        return a * Double(b)
+    }
+    
+    /// Integer multiplication..
+    /// - Parameters:
+    ///   - a: Integer number.
+    ///   - b: Real number.
+    /// - Returns: Raises `a` to the power of `b`.
+    @inlinable
+    public static func * (_ a: Int, _ b: Double) -> Double {
+        return Double(a) * b
+    }
+    
+    /// Integer division..
+    /// - Parameters:
+    ///   - a: Real number.
+    ///   - b: Integer number.
+    /// - Returns: Raises `a` to the power of `b`.
+    @inlinable
+    public static func / (_ a: Double, _ b: Int) -> Double {
+        return a / Double(b)
+    }
+    
+    /// Integer division.
+    /// - Parameters:
+    ///   - a: Integer number.
+    ///   - b: Real number.
+    /// - Returns: Raises `a` to the power of `b`.
+    @inlinable
+    public static func / (_ a: Int, _ b: Double) -> Double {
+        return Double(a) / b
+    }
+    
+}
+*/
