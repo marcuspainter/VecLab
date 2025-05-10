5f6d48328548f99114d9524c15588f42e1c848df
Marcus Painter <marcus@marcuspainter.com>
2025-05-10 16:03:10 +0100

    Update ComplexDoubleArray+Math+Unary.swift

--- Sources/VecLab/Types/ComplexArray/ComplexDoubleArray+Math+Unary.swift
+++ Sources/VecLab/Types/ComplexArray/ComplexDoubleArray+Math+Unary.swift
@@ -12,9 +12,21 @@ extension ComplexDoubleArray {
     /// Unary minus.
     /// - Parameter a: Complex array.
     /// - Returns: The result of -a.
+    //public static prefix func - (a: ComplexDoubleArray) -> ComplexDoubleArray {
+    //    let real = vDSP.negative(a.real)
+    //    let imag = vDSP.negative(a.imag)
+    //    return ComplexDoubleArray(real, imag)
+   // }
+    
+    
     public static prefix func - (a: ComplexDoubleArray) -> ComplexDoubleArray {
-        let real = vDSP.negative(a.real)
-        let imag = vDSP.negative(a.imag)
-        return ComplexDoubleArray(real, imag)
+        var c = ComplexDoubleArray(count: a.count)
+        ComplexDoubleArray.withUnsafeParameters(a, &c) { A, C, N in
+            vDSP_zvnegD(A, 1, C, 1, N)
+        }
+        return c
     }
 }
+
+
+
