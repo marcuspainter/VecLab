699bc755373c884745ed0ae551f5b34b6357e5b2
Marcus Painter <marcus@marcuspainter.com>
2025-05-11 23:33:01 +0100

    Fill and clear functions updated

--- Sources/VecLab/Functions/Arrays/flip.swift
+++ Sources/VecLab/Functions/Arrays/flip.swift
@@ -27,11 +27,9 @@ public func flip(_ x: RealArray) -> RealArray {
 public func flip(_ x: ComplexArray) -> ComplexArray {
     validateSize(x)
     // Copy
-    var yreal = x.real
-    var yimag = x.imag
-
+    var c = x
     // Inplace reverse
-    vDSP.reverse(&yreal)
-    vDSP.reverse(&yimag)
-    return ComplexArray(yreal, yimag)
+    vDSP.reverse(&c.real)
+    vDSP.reverse(&c.imag)
+    return c
 }
