53eca9b6af210a5b092fbf771d91e98bf4d483a0
Marcus Painter <marcus@marcuspainter.com>
2025-09-23 00:39:28 +0100

    Power operators file

--- Sources/VecLab/Types/Operators/PowerOperators.swift
+++ Sources/VecLab/Types/Operators/PowerOperators.swift
@@ -5,5 +5,14 @@
 //  Created by Marcus Painter on 16/09/2025.
 //
 
-/// Power
+// Uses Matlab convention: 2**2**3 Matlab = 64, NumPy = 256
+precedencegroup PowerPrecedence {
+    higherThan: MultiplicationPrecedence
+    // NumPy
+    associativity: right
+    // Matlab
+    // associativity: left
+}
+
+/// Power operator.
 infix operator ** : PowerPrecedence
